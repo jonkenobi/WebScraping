03c21e2f566f9cfe9a737d9f1e3ef63afcd34311
jonkenobi <eragon.pokemon@hotmail.com>
2024-07-20 15:33:31 +0900

    20240720 Update - Scrape duration Overall update to the code so it is usable again and improved the README.md

--- README.md
+++ README.md
@@ -1,18 +1,24 @@
-This python script scrapes the distance and duration for a map route, given origin and destination. 
+# Map route duration scraper
+This python script scrapes the time duration it takes for a map route, given the origin and destination.
+It can output those values into an Excel file
 
 ## How to use
-1. Create an Excel sheet with the same columns as defined in input.xlsx.
-2. Replace C1-E1 with the 3 addresses you want to lookup.
-3. Run distance_scrape_with_excel.py
-4. The output will be created in output.xlsx
+1. Create an Excel sheet similar to input.xlsx. Write Origins from B1 rightwards, destinations from A2 downwards
+2. Run distance_scrape_with_excel.py
+3. The output will be created in output.xlsx
 
-## Preqrequisites to Install
+For modifying and testing, use `distance_scrape_with_excel.ipynb`. 
+`distance_scrape_with_excel.py` is an excerpt of the code from `distance_scrape_with_excel.ipynb`
+for convenience of use if you just want to run the script without starting Jupyter Notebook
+
+## Requirements
+
 - Python
 - Python packages: selenium, openpyxl  (Download with pip or conda)
-- Chromedriver (Have chromedriver.exe) 
+- Chromedriver (Put chromedriver.exe in the rootdirectory) 
 
 ## Technical tool considerations
-* Python is one of the best and easiest web scraping languages. Besides that, it has a simple syntax and is fast to develop, suitable for our non-complex mini project 
+* Python is one of the best and easiest web scraping languages. Besides that, it has a simple syntax and is fast to develop, suitable for this non-complex mini project 
 * Tried to use the requests_html library, but had difficulty rendering the js of our particular Third Party map service so resorted to Selenium. 
 
 ## Future Improvements
--- distance_scrape_with_excel.py
+++ distance_scrape_with_excel.py
@@ -4,10 +4,8 @@
 # In[1]:
 
 
-#Import packages 
+# Import packages
 import time
-import re
-import string 
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -17,60 +15,77 @@ from selenium.webdriver.support.expected_conditions import presence_of_element_l
 from openpyxl import Workbook, load_workbook
 from openpyxl.utils import get_column_letter
 
-
 # In[2]:
 
 
-#Load workbook data
-wb = load_workbook(filename = 'input.xlsx')
+# Load workbook data
+wb = load_workbook(filename='input.xlsx')
 sheet = wb.active
-
 
 # In[3]:
 
 
-#Prepare destinations
+# Create array of destinations extracted from the A2-A15 in Excel
 destinations = []
-for cell in ['C1','D1','E1']:
-    destinations.append(sheet[cell].value)
+for cell in list(range(2, 16)):
+    destinations.append(sheet[f"A{cell}"].value)
 
 
 # In[4]:
 
 
-def scrape_distance(driver):
-    wait.until(presence_of_element_located((By.CSS_SELECTOR, "#pane > div > div.widget-pane-content.cYB2Ge-oHo7ed > div > div > div.section-layout")))
-    distances = driver.find_elements_by_class_name('section-directions-trip-distance')
-    distance = re.sub("[^0-9.]", "", distances[0].text) #Take first distance
-    return distance
+# Helper functions
+
+def click_train_btn(driver: webdriver.Chrome, wait: WebDriverWait):
+    ## Find train btn
+    wait.until(
+        presence_of_element_located(
+            (By.XPATH, '/html/body/div[2]/div[3]/div[8]/div[3]/div[1]/div[2]/div/div[2]/div/div/div/div[3]/button')))
+    btn = driver.find_elements_by_xpath(
+        '/html/body/div[2]/div[3]/div[8]/div[3]/div[1]/div[2]/div/div[2]/div/div/div/div[3]/button')[0]
+    ## Click train btn
+    btn.click()
+
+
+def scrape_duration(wait: WebDriverWait):
+    ## Get time duration in 1st suggested trip, by using the XPATH of the 1st time element
+    d1 = wait.until(
+        presence_of_element_located((By.XPATH, '//*[@id="section-directions-trip-0"]/div[1]/div/div[1]/div')))
+
+    return d1.text
 
 
 # In[5]:
 
 
-#Start web scraping
+# Start web scraping
 print("Starting Chrome....")
+
 with webdriver.Chrome() as driver:
-    wait = WebDriverWait(driver, 10)
     startTime = time.time()
-    
-    row = 2  #starting from row 2 in excel
-    origin_zipcode = ""
-    NUMBER_OF_DESTINATION_COLUMNS = 3
-    
-    while row < 501:
-        origin_zipcode = sheet['B'+ str(row)].value
-        if origin_zipcode != None:
-            for i in range(NUMBER_OF_DESTINATION_COLUMNS): 
-                driver.get(f"https://www.google.com/maps/dir/Japan,+〒{origin_zipcode}/{destinations[i]}")
-                walking_button = driver.find_elements_by_xpath('//*[@id="omnibox-directions"]/div/div[2]/div/div/div[1]/div[4]/button')[0]
-                walking_button.click() ## Click the walking btn 
-                sheet[string.ascii_uppercase[i+2]+ str(row)] = scrape_distance(driver)
+    wait = WebDriverWait(driver, 10)
+
+    col_num = 2  # starting from column B in excel
+
+    while True:
+        col = get_column_letter(col_num)
+        origin = sheet[f"{col}1"].value  # Get origin
+        if origin != None:
+            for i in range(len(destinations)):
+                directions_url = f"https://www.google.com/maps/dir/{origin}/{destinations[i]}"
+                driver.get(directions_url)  # Open directions url
+
+                click_train_btn(driver, wait)
+                time.sleep(0.1)  # wait for new trip results after clicking train btn
+
+                duration = scrape_duration(wait)
+                print(f"{directions_url}: {duration}")
+
+                # Write duration into excel
+                sheet[col + str(i + 2)] = duration
         else:
             break
-        sheet['F'+ str(row)] = "https://www.google.com/maps/place/〒"+ origin_zipcode
-        row+=1    
-
+        col_num += 1
+    print("Total time spent scraping is ", time.time() - startTime, "secs")
     wb.save("./output.xlsx")
-    print("Total time spent is ",time.time()-startTime, "secs")
 
